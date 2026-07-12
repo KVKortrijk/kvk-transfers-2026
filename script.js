ee48303e99fbd86e38187914bf6388100347acfd
KVKortrijk <demat25bjorn@gmail.com>
2026-07-12 16:02:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,64 @@ const transfers = [
     fee: "Free Transfer",
     club: "Maccabi Haifa",
     image: "images/podgoreanu.png"
+},
+
+{
+    direction: "incoming",
+    category: "Bought",
+    name: "Boris Lambert",
+    position: "CDM / CB",
+    value: "€800k",
+    fee: "€475k (Loan Triggered)",
+    club: "Willem II",
+    image: "images/boris-lambert.png"
+},
+
+{
+    direction: "incoming",
+    category: "On Loan",
+    name: "Ken Masui",
+    position: "LM / CAM / RM",
+    value: "€900k",
+    fee: "Loan (Option to Buy: YES)",
+    club: "Nagoya Grampus",
+    image: "images/ken-masui.png"
+},
+
+{
+    direction: "incoming",
+    category: "On Loan",
+    name: "Harrison Murray-Campbell",
+    position: "CB / RB / LB",
+    value: "€500k",
+    fee: "Loan (Option to Buy: NO)",
+    club: "Chelsea",
+    image: "images/harrison-murray-campbell.png"
+},
+
+{
+    direction: "incoming",
+    category: "Extended",
+    name: "Brecht Dejaegere",
+    position: "CM / CDM / CAM",
+    value: "€300k",
+    fee: "Contract Extension (1 year)",
+    club: "KV Kortrijk",
+    image: "images/brecht-dejaegere.png"
+},
+
+{
+    direction: "incoming",
+    category: "Returning",
+    name: "Mohamed Boussadia",
+    position: "CAM / CM",
+    value: "N/A",
+    fee: "Returned from Loan",
+    club: "R Knokke FC",
+    image: "images/mohamed-boussadia.png"
 }
+
+];
 
 ];
 
